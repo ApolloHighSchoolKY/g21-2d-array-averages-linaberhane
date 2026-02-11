ebb66c654162300d7b8ac6bc34aab8823a25b7b0
linaberhane <lina.berhane@stu.daviess.kyschools.us>
2026-02-11 21:51:30 +0000

    Commit

--- ArrayAverages.java
+++ ArrayAverages.java
@@ -1,28 +1,69 @@
 import java.util.Arrays;
 
-public class ArrayAverages{
+public class ArrayAverages
+{
   
   int[][] ray;
 
-  public ArrayAverages(int[][] rayray){
+  public ArrayAverages(int[][] rayray)
+  {
     ray = rayray;
   }
 
-  public void rowAverages(){
-    
+  public void rowAverages()
+  {
+    double sum = 0;
+
+    for(int row=0; row<ray.length; row++)
+    {
+      sum = 0;
+
+      for(int col=0; col<ray[row].length; col++)
+      {
+        sum+=ray[row][col];
+      }
+      double rowAvg = sum/ray[0].length;
+      System.out.println("The average of row " + row + " is: " +  rowAvg);
+    }
   }
 
-  public void columnAverages(){
+  public void columnAverages()
+  {
     //Column major order
     //   Take each column and go through all of the rows
     //   Top down and Left to Right
+    double sum = 0;   
+
+    for(int col=0; col<ray[0].length; col++)
+    {
+      sum = 0;
+
+      for(int row=0; row<ray.length; row++)
+        {
+          sum+=ray[row][col];
+        }
+      double colAvg = sum/ray.length;
+      System.out.println("The average of column " + col + " is: " +  colAvg);
+    }
   }
 
-  public void arrayAverage(){
+  public void arrayAverage()
+  {
+    double sum = 0;
 
+    for(int row=0; row<ray.length; row++)
+    {
+      for(int col=0; col<ray[row].length; col++)
+      {
+        sum+=ray[row][col];
+      }
+    }
+    double rayAvg = sum/(ray.length*ray[0].length);
+    System.out.println("The average of the array is: " +  rayAvg);
   }
 
-  public String toString(){
+  public String toString()
+  {
     for(int row=0; row<ray.length; row++)
     {
       System.out.println(Arrays.toString(ray[row]));
--- Gee21.java
+++ Gee21.java
@@ -9,13 +9,25 @@ public class Gee21{
       8  9  10 11 12 13 14
       15 16 17 18 19 20 21
       22 23 24 25...
-      */
+    */
+
+    int val = 1;
+    for(int row=0; row<ray.length; row++)
+    {
+      for(int col=0; col<ray[row].length; col++)
+      {
+        ray[row][col] = val;
+        val++;
+      }
+    }
 
     ArrayAverages twoDee = new ArrayAverages(ray);
 
     System.out.println(twoDee);
     twoDee.rowAverages();
+    System.out.println("");
     twoDee.columnAverages();
+    System.out.println("");
     twoDee.arrayAverage();
   }
 }
